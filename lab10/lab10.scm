d9b0d29edd4fac82b6050e451d4d246679f25774
songmuhan <songmuhan@foxmail.com>
2022-09-12 22:39:40 +0800

    finish lab10

--- lab10/lab10.scm
+++ lab10/lab10.scm
@@ -6,25 +6,32 @@
   (cons name (cons class students)))
 
 (define (student-get-name student)
-  'YOUR-CODE-HERE)
+  (car student))
 
 (define (student-get-classes student)
-  'YOUR-CODE-HERE)
+  (cdr student))
 
 (define (teacher-get-name teacher)
-  'YOUR-CODE-HERE)
+  (car teacher))
 
 (define (teacher-get-class teacher)
-  'YOUR-CODE-HERE)
+  (car(cdr teacher)))
 
 (define (teacher-get-students teacher)
-  'YOUR-CODE-HERE)
+  (cdr (cdr teacher)))
 
 (define (student-attend-class student class)
-  'YOUR-CODE-HERE)
+  (student-create (student-get-name student) (cons class (student-get-classes student))))
+
 
 (define (teacher-hold-class teacher)
-  'YOUR-CODE-HERE)
+  (define (student-attend-specific-class student)(student-attend-class student (teacher-get-class teacher)))
+  (teacher-create 
+    (teacher-get-name teacher) 
+    (teacher-get-class teacher) 
+    (map student-attend-specific-class (teacher-get-students teacher))
+  )
+)
 
 ; Rational Abstraction
 ; Helpers
